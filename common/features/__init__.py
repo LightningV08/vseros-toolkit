e6a6d420a2042b17083147b9b967d9b97623d9db
Artyom Konukhov <93391108+IceDarold@users.noreply.github.com>
2025-11-17 13:02:03 +0300

    Add geospatial grid and neighbor feature builders

--- common/features/__init__.py
+++ common/features/__init__.py
@@ -1,6 +1,6 @@
 from .assemble import make_dense, make_sparse
 from .store import FeatureStore
-from . import cat_freq, cat_te_oof, num_basic, text_tfidf
+from . import cat_freq, cat_te_oof, geo_grid, geo_neighbors, num_basic, text_tfidf
 from .types import FeaturePackage, Kind
 
 __all__ = [
@@ -10,6 +10,8 @@ __all__ = [
     "num_basic",
     "cat_freq",
     "cat_te_oof",
+    "geo_grid",
+    "geo_neighbors",
     "text_tfidf",
     "make_dense",
     "make_sparse",
